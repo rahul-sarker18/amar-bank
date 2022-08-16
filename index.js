3404b05380aac8a3dd221462e9a7e0613436d630
rahul-sarker18 <rahulsarker1495@gmail.com>
2022-08-16 23:52:01 +0530

    new balance add and creant total balance

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 2.5 . clear the depojit input field after geating the value
 4. privas dipojit total
 5. calclulate new depojit total and set the value to the depojit total
+6.balance  amount slect  and type cange
+
 
 */
 
@@ -23,4 +25,14 @@ document.getElementById('depojit-btn').addEventListener('click', function(){
     // step5
     const newDepojitTotal = newDepojitAmount + privasDepojitTotal;
     depojitTotalFil.innerText = newDepojitTotal;
+
+    // step6
+    const balanceTotalElement = document.getElementById('balance-total');
+    const balanceTotalElementString = balanceTotalElement.innerText;
+    const balanceNumber = parseFloat(balanceTotalElementString);  
+    
+    // step7
+    const totalBalance = newDepojitAmount + balanceNumber;
+    balanceTotalElement.innerText = totalBalance;
+
 })
